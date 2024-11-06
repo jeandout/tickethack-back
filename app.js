517b4a89e30cbde7407d359428d76fb8c7906847
jeandout <jeandout@hotmail.fr>
2024-11-06 15:02:52 +0100

    ajout de cors

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const cors = require('cors');
+app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/trip', tripRouter)
 module.exports = app;
